24713cbb5faad788c36dc10de2df20a9cfe8c70d
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-06-03 07:42:49 +0000

    Done

--- react-blog-app/src/components/Header.jsx
+++ react-blog-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-yellow-100 p-2 shadow-slate-400 shadow text-center sticky top-0'>
+    <div className='bg-amber-200 p-2 shadow-slate-400 shadow text-center sticky top-0'>
       <h1 className='text-xl font-semibold'>HITMAN BLOGS</h1>
     </div>
   )
